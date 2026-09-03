916d2da331f503fdf54bb40568f29bb4a5933689
Pedro Mariano Vieira Nolasco <marianopedro237@gmail.com>
2026-09-02 21:21:17 -0300

    Add files via upload

---
+++ questao 1.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+main(){
+	int a,b,c,soma;
+
+	
+	printf("Digite o valor a:");
+	scanf("%d",&a);
+	printf("Digite o valor b");
+	scanf("%d",&b);
+	printf("Digite o valor c ");
+	scanf("%d",&c);
+	
+	soma= a+b;
+	
+	
+	if( soma<c){
+		printf(" soma e menor" );
+		
+	}else if (soma<=c){
+		
+		printf(" soma maior");
+		
+	}
+		
+		
+	
+		
+	}
+	
+
+	
+	
+	
+		
+	
+
+	
+
