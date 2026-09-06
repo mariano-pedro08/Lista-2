7f60d6efc997909a6014b52fb1ae558a9265d91f
Pedro Mariano Vieira Nolasco <marianopedro237@gmail.com>
2026-09-06 00:18:41 -0300

    Add files via upload

---
+++ questão 4.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+main (){
+	
+	int A,B,C;
+	
+	printf("Digite o valor de A");
+	scanf("%i",&A);
+	printf("Digite o valor de B");
+	scanf("%i",&B);
+	
+
+	
+	if(A==B){
+	C=A+B;
+    } else {
+     C=A*B;
+ 
+
+   printf("\n Resultado final (C):%d\n",C);
+   
+}
+
+
+	return 0;
+	
+}
+		
