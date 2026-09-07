227e192f4d212c8080b1d0ab5b88b631de1e43b0
Pedro Mariano Vieira Nolasco <marianopedro237@gmail.com>
2026-09-07 12:21:17 -0300

    Add files via upload

---
+++ questão 7.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+main (){
+	
+	int numero_inteiro,resultado;
+	
+	printf("Digite o numero inteiro");
+	scanf("%i",&numero_inteiro);
+	
+if (numero_inteiro % 2 == 0) {
+        resultado = numero_inteiro * 5;
+        printf("\nO numero %d e par. Resultado (*5): %d\n", numero_inteiro, resultado);
+    } else {
+        resultado = numero_inteiro + 8;
+        printf("\nO numero %d e impar. Resultado (+8): %d\n", numero_inteiro, resultado);
+    }
+
+     
+  return 0;
+}
